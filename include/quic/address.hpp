6d2585543593a6d807489efafccda75f5038b5ca
dr7ana <danielrouhana@gmail.com>
2023-12-07 14:19:45 -0800

    Address:set_port

--- include/quic/address.hpp
+++ include/quic/address.hpp
@@ -59,6 +59,22 @@ namespace oxen::quic
             return *this;
         }
 
+        void set_port(uint16_t port)
+        {
+            if (_sock_addr.ss_family == AF_INET6)
+            {
+                auto& sin6 = reinterpret_cast<sockaddr_in6&>(_sock_addr);
+                sin6.sin6_port = oxenc::host_to_big(port);
+            }
+            else if (_sock_addr.ss_family == AF_INET)
+            {
+                auto& sin4 = reinterpret_cast<sockaddr_in&>(_sock_addr);
+                sin4.sin_port = oxenc::host_to_big(port);
+            }
+            else
+                throw std::invalid_argument{"Error: could not set port in unknown sock_addr"};
+        }
+
         // Converts an IPv4 address into an IPv4-mapped IPv6 address.  The address must already be
         // an IPv4 address (throws if not).
         void map_ipv4_as_ipv6();
