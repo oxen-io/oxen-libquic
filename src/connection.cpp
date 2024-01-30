35cbe517803eef92bea0e184a47c7b99e02ff751
Jason Rhinelander <jason@imaginary.ca>
2024-01-30 13:40:55 -0400

    Disable log statement about no early data
    It fires on every connection currently, because we don't yet support
    0rtt at all and so is just unwanted log spam.

--- src/connection.cpp
+++ src/connection.cpp
@@ -373,16 +373,16 @@ namespace oxen::quic
             Moreover, decoding and setting 0RTT transport parameters must be handled in connection creation. Both that
             location and the required callbacks are comment-blocked in the relevant location.
         */
-        if (not tls_session->get_early_data_accepted())
-        {
-            log::info(log_cat, "Early data was rejected by server!");
+        // if (not tls_session->get_early_data_accepted())
+        //{
+        // log::info(log_cat, "Early data was rejected by server!");
 
-            // if (auto rv = ngtcp2_conn_tls_early_data_rejected(conn.get()); rv != 0)
-            // {
-            //     log::error(log_cat, "ngtcp2_conn_tls_early_data_rejected: {}", ngtcp2_strerror(rv));
-            //     return -1;
-            // }
-        }
+        // if (auto rv = ngtcp2_conn_tls_early_data_rejected(conn.get()); rv != 0)
+        // {
+        //     log::error(log_cat, "ngtcp2_conn_tls_early_data_rejected: {}", ngtcp2_strerror(rv));
+        //     return -1;
+        // }
+        //}
 
         // ustring data;
         // data.resize(256);
