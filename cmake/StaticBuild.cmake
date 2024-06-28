1d4d92ad10304715b5c8e977e7c5cce885646e15
Audric Ackermann <audric@loki.network>
2024-06-28 15:05:46 +1000

    fix: add back some deps, still without gmp

--- cmake/StaticBuild.cmake
+++ cmake/StaticBuild.cmake
@@ -278,14 +278,54 @@ if(CMAKE_C_COMPILER_ID STREQUAL GNU)
     set(libtasn_extra_cflags " -Wno-analyzer-null-dereference -Wno-analyzer-use-of-uninitialized-value")
 endif()
 
+
+# build_external(libtasn1
+#    CONFIGURE_COMMAND ./configure ${build_host} --disable-shared --disable-doc --prefix=${DEPS_DESTDIR} --with-pic
+#         "CC=${deps_cc}" "CXX=${deps_cxx}"
+#         "CFLAGS=${deps_CFLAGS}${apple_cflags_arch}${libtasn_extra_cflags}"
+#         "CXXFLAGS=${deps_CXXFLAGS}${apple_cflags_arch}${libtasn_extra_cflags}"
+#         "CPPFLAGS=-I${DEPS_DESTDIR}/include" "LDFLAGS=-L${DEPS_DESTDIR}/lib${apple_ldflags_arch}" ${cross_rc}
+#     BUILD_BYPRODUCTS ${DEPS_DESTDIR}/lib/libtasn1.a ${DEPS_DESTDIR}/include/libtasn1.h)
+# add_static_target(libtasn1::libtasn1 libtasn1_external libtasn1.a)
+
+# build_external(libiconv
+#     CONFIGURE_COMMAND ./configure ${build_host} --disable-shared --prefix=${DEPS_DESTDIR} --with-pic
+#         "CC=${deps_cc}" "CXX=${deps_cxx}" "CFLAGS=${deps_CFLAGS}${apple_cflags_arch}" "CXXFLAGS=${deps_CXXFLAGS}${apple_cflags_arch}"
+#         "CPPFLAGS=-I${DEPS_DESTDIR}/include" "LDFLAGS=-L${DEPS_DESTDIR}/lib${apple_ldflags_arch}" ${cross_rc}
+#     BUILD_BYPRODUCTS ${DEPS_DESTDIR}/lib/libiconv.a ${DEPS_DESTDIR}/include/iconv.h)
+# add_static_target(libiconv::libiconv libiconv_external libiconv.a)
+
+# build_external(libunistring
+#     CONFIGURE_COMMAND ./configure ${build_host} --disable-shared --prefix=${DEPS_DESTDIR} --with-pic
+#         "CC=${deps_cc}" "CXX=${deps_cxx}" "CFLAGS=${deps_CFLAGS}${apple_cflags_arch}" "CXXFLAGS=${deps_CXXFLAGS}${apple_cflags_arch}"
+#         "CPPFLAGS=-I${DEPS_DESTDIR}/include" "LDFLAGS=-L${DEPS_DESTDIR}/lib${apple_ldflags_arch}" ${cross_rc}
+#     DEPENDS libiconv_external
+#     BUILD_BYPRODUCTS ${DEPS_DESTDIR}/lib/libunistring.a ${DEPS_DESTDIR}/include/unistr.h)
+# add_static_target(libunistring::libunistring libunistring_external libunistring.a libiconv::libiconv)
+
+# build_external(libidn2
+#     CONFIGURE_COMMAND ./configure ${build_host} --disable-shared --disable-doc --prefix=${DEPS_DESTDIR} --with-pic
+#         "CC=${deps_cc}" "CXX=${deps_cxx}" "CFLAGS=${deps_CFLAGS}${apple_cflags_arch}" "CXXFLAGS=${deps_CXXFLAGS}${apple_cflags_arch}" ${cross_rc}
+#     DEPENDS libunistring_external
+#     BUILD_BYPRODUCTS ${DEPS_DESTDIR}/lib/libidn2.a ${DEPS_DESTDIR}/include/idn2.h)
+# add_static_target(libidn2::libidn2 libidn2_external libidn2.a libunistring::libunistring)
+
+# build_external(gmp
+#     CONFIGURE_COMMAND ./configure ${build_host} --disable-shared --prefix=${DEPS_DESTDIR} --with-pic
+#         "CC=${deps_cc}" "CXX=${deps_cxx}" "CFLAGS=${deps_CFLAGS}${apple_cflags_arch}" "CXXFLAGS=${deps_CXXFLAGS}${apple_cxxflags_arch}"
+#         "LDFLAGS=-L${DEPS_DESTDIR}/lib${apple_ldflags_arch}" ${cross_rc} CC_FOR_BUILD=cc CPP_FOR_BUILD=cpp
+#     DEPENDS libidn2_external libtasn1_external
+# )
+# add_static_target(gmp::gmp gmp_external libgmp.a libidn2::libidn2 libtasn1::libtasn1)
+
 build_external(nettle
-    CONFIGURE_COMMAND ./configure ${build_host} --disable-shared --prefix=${DEPS_DESTDIR} --libdir=${DEPS_DESTDIR}/lib
-        --enable-pic --disable-openssl --disable-documentation --enable-mini-gmp
+    CONFIGURE_COMMAND ./configure ${build_host} --disable-shared  --prefix=${DEPS_DESTDIR} --libdir=${DEPS_DESTDIR}/lib
+        --enable-pic --disable-openssl --disable-documentation --enable-mini-gmp --without-idn
         "CC=${deps_cc}" "CXX=${deps_cxx}"
         "CFLAGS=${deps_CFLAGS}${apple_cflags_arch}" "CXXFLAGS=${deps_CXXFLAGS}${apple_cxxflags_arch}"
         "CPPFLAGS=-I${DEPS_DESTDIR}/include"
         "LDFLAGS=-L${DEPS_DESTDIR}/lib${apple_ldflags_arch}"
-
+        # DEPENDS libidn2_external
     BUILD_BYPRODUCTS
     ${DEPS_DESTDIR}/lib/libnettle.a
     ${DEPS_DESTDIR}/lib/libhogweed.a
@@ -295,7 +335,7 @@ add_static_target(nettle::nettle nettle_external libnettle.a)
 add_static_target(hogweed::hogweed nettle_external libhogweed.a nettle::nettle)
 
 build_external(gnutls
-    CONFIGURE_COMMAND ./configure ${build_host} --disable-shared --with-nettle-mini --without-libidn2 --with-included-unistring  --with-included-libtasn1 --prefix=${DEPS_DESTDIR} --with-pic
+    CONFIGURE_COMMAND ./configure ${build_host} --disable-shared --with-nettle-mini  --with-included-unistring   --prefix=${DEPS_DESTDIR} --with-pic --with-included-libtasn1
         --without-p11-kit --disable-libdane --disable-cxx --without-tpm --without-tpm2 --disable-doc
         --without-zlib --without-brotli --without-zstd --without-libintl-prefix --disable-tests
         --disable-valgrind-tests --disable-full-test-suite
